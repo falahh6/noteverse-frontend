e5d4b9b7ef16cd754606a579572c5d4f7e345327
falahh6 <falahsss900@gmail.com>
2024-11-24 15:09:08 +0530

    feat : google auth

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,9 +11,10 @@ import MaxWidthWrapper from '@/components/layout/MaxwidthWrapper'
 import Link from 'next/link'
 import * as yup from 'yup'
 import { toast } from 'sonner'
-import { useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import { SendVerificationEmail } from '@/lib/api'
 import { Button } from '@/components/ui/button'
+import { Icons } from '@/components/icons'
 
 interface IFormInput {
   name: string
@@ -170,7 +171,10 @@ const Page = () => {
           )}
 
           <button
-            className="bg-gradient-to-br group relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            className="bg-gradient-to-br text-sm group relative group/btn from-black dark:from-zinc
+            -900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-m
+            edium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px
+            _0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
           >
             Sign Up{' '}
@@ -182,26 +186,53 @@ const Page = () => {
             <BottomGradient />
           </button>
 
-          <div className="mt-4 w-full flex items-center justify-center">
+          {/* <div className="mt-4 w-full flex items-center justify-center">
             <p className="text-sm font-semibold text-gray-600">
               Already have an account?{' '}
               <span className="text-blue-500  transition-[text-decoration-line] duration-300 ease-in-out hover:underline">
                 <Link href={'/signin'}>Login!</Link>
               </span>
             </p>
-          </div>
+          </div> */}
         </form>
-        {/* <Button
-          onClick={() => {
-            SendVerificationEmail(
-              'falahsss900@gmail.com',
-              'Verify your email',
-              window.location.host + `/verify?vid=hey`,
-            )
-          }}
+        <div className="flex flex-row justify-center items-center">
+          <div
+            className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to
+-transparent my-4 h-[1px] w-full"
+          />
+          <span className="text-xs">or</span>
+          <div
+            className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to
+-transparent my-4 h-[1px] w-full"
+          />
+        </div>
+        <button
+          className="border relative group/btn flex space-x-2 w-full items-center justify-center px-4 w-fu
+ll text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_
+0px_1px_1px_var(--neutral-800)]"
+          onClick={() => signIn('google')}
         >
-          hey
-        </Button> */}
+          <Icons.google
+            width={20}
+            height={20}
+            className="text-neutral-800 dark:text-neutral-300"
+          />
+          <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+            Continue with Google
+          </span>
+          <BottomGradient />
+        </button>
+        <div className="mt-4 w-full flex items-center justify-center">
+          <p className="text-sm font-semibold text-gray-600">
+            Already have an account?{' '}
+            <span
+              className="text-blue-500  transition-[text-decoration-line] duration-300 ease-in-
+out hover:underline"
+            >
+              <Link href={'/signin'}>Login!</Link>
+            </span>
+          </p>
+        </div>
       </div>
     </MaxWidthWrapper>
   )
